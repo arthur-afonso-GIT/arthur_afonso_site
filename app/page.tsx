42d8ef35092d271d77e3c1e38b41e7bab930c865
arthurafonso <afaa@cesar.school>
2026-08-18 16:44:07 -0300

    Align social links with introduction

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,6 @@ export default function Home() {
         <p className="lead">Estudante de Sistemas de Informação, atualmente no 2º período, com interesse em Inteligência Artificial, LLMs, Dados e Engenharia de Software. Entusiasta de tecnologia e desenvolvimento de software, busco aprofundar meus conhecimentos por meio de projetos práticos, explorando a construção de aplicações, soluções baseadas em IA e sistemas orientados a dados.</p>
       </section>
       <aside className="socials" aria-label="Redes sociais">
-        <p>Encontre-me</p>
         <a href="https://github.com/arthur-afonso-GIT" target="_blank" rel="noreferrer"><span>GitHub</span><i>↗</i></a>
         <a href="https://www.linkedin.com/in/arthur-flor%C3%AAncio-afonso/" target="_blank" rel="noreferrer"><span>LinkedIn</span><i>↗</i></a>
       </aside>
